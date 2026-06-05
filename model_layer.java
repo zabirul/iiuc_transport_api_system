c5de2116c2a6e1415e722311a478ae6e67f0cda9
Pritam Barua <pbarua716@gmail.com>
2026-06-06 00:15:55 +0600

    Update model_layer.java

--- model_layer.java
+++ model_layer.java
@@ -1,75 +1,77 @@
-package com.iiuc.transport.model;
+package Model_layer;
 
-public class TransportSchedule {
+public class Bus {
+  // bus er information store kora
+     private Long id;
+     private String busNumber;
+     private String busDriverName;
+     private String routeName;
+     private String busSchedule;
+     private int capacity;
 
-    // bus er information store kora
-    private Long id;
-    private String busNumber;
-    private String routeName;
-    private String driverName;
-    private Integer capacity;
-    private String schedule;     
-    private String semester;      
+ // Default Constructor
+      public Bus() {
+ }   // Parameterized Constructor
    //parameter charai object create korar jonno, eita na thakle object create kora jabe na error dekhabe entity object create korar jonno ei no argument constructor ta lage
-    public TransportSchedule() {}
-   //parameterized constructor sob man initialize korar jonno
-    public TransportSchedule(Long id, String busNumber, String routeName,String driverName, Integer capacity,String schedule, String semester) {
-       //object er bitor data boshano hoi
-        this.id = id;
-        this.busNumber = busNumber;
-        this.routeName = routeName;
-        this.driverName = driverName;
-        this.capacity = capacity;
-        this.schedule = schedule;
-        this.semester = semester;
-    }
+ public Bus(Long id, String busNumber, String busDriverName,
+       String routeName, String busSchedule, int capacity) {
+       //parameterized constructor sob man initialize korar jonno
+     this.id = id;
+     this.busNumber = busNumber;
+     this.busDriverName = busDriverName;
+     this.routeName = routeName;
+     this.busSchedule = busSchedule;
+     this.capacity = capacity;
+ }
+
+ // Getter and Setter ID
    //bair theke paoa read ba access korar jonno ekhane get ba id er value access korbe
     public Long getId() {
-         return id; 
-        }
-    public void setId(Long id) { 
+        return id;
+    }
+//get korar por set korar jonno
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    // Get and Set Bus Number
+     public String getBusNumber() {
+       return busNumber;
+    }
+
+    public void setBusNumber(String busNumber) {
+      this.busNumber = busNumber;
+    }
+
+    // Getter and Setter BusDriver Name
+    public String getBusDriverName() {
+        return busDriverName;
+      }
+    public void setBusDriverName(String busDriverName) {
+     this.busDriverName = busDriverName;
+    }
+
+    // Getter and Setter Route Name
+     public String getRouteName(){
+      return routeName;
      }
 
-    public String getBusNumber() { 
-        return busNumber; 
-    }
-    public void setBusNumber(String busNumber) { 
-        this.busNumber = busNumber;
-    }
+    public void setRouteName(String routeName){
+     this.routeName = routeName;
+     }
+// Getter and Setter Bus Schedule
+     public String getBusSchedule()  {
+      return busSchedule;
+     }
 
-    public String getRouteName() { 
-        return routeName; 
-    }
-    public void setRouteName(String routeName) { 
-        this.routeName = routeName;
-    }
-
-    public String getDriverName() {
-         return driverName; 
-        }
-    public void setDriverName(String driverName) {
-         this.driverName = driverName; 
-        }
-
-    public Integer getCapacity() {
-         return capacity; 
-        }
-    public void setCapacity(Integer capacity) {
-         this.capacity = capacity; 
-        }
-
-    public String getSchedule() { 
-        return schedule; 
-    }
-    public void setSchedule(String schedule) { 
-        this.schedule = schedule;
-    }
-
-    public String getSemester() { 
-        return semester; 
-    }
-    public void setSemester(String semester) { 
-        this.semester = semester;
+    public void setBusSchedule(String busSchedule){
+      this.busSchedule = busSchedule;
+     }
+// Getter and Setter for Capacity
+     public int getCapacity(){
+      return capacity;
+     }
+    public void setCapacity(int capacity){
+      this.capacity = capacity;
      }
 }
